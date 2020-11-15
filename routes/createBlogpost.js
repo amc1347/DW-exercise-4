63674eea42b1ac1801060cd02ab9c95bbda7469a
A <alisonchen@a.local>
2020-11-15 00:31:37 -0500

    commit 8

--- routes/createBlogpost.js
+++ routes/createBlogpost.js
@@ -1,9 +1,24 @@
-// show one blog post
+// create blog post
 
 const express = require("express");
-
 const router = express.Router();
 
-router.get("/", (req, res) => res.send("Create a blogpost"));
+const firebase = require("firebase");
+
+const db = firebase.firestore();
+
+const blogposts = db.collection("blogposts");
+
+const form = `
+<form>
+    <input type="text" name ="title" placeholder="Title of Post" />
+    <input type="text" name ="text" placeholder="Text of Post" />
+    <input type="text" name ="author" placeholder="Author" />
+    <button type="submit">Submit Post</button>
+
+</form>
+    `;
+
+router.get("/", (req, res) => res.send(form));
 
 module.exports = router;
